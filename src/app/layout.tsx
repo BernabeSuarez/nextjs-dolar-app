b1046d42a08979b903487bfca02410361570ccf1
Bernabe Suarez <bernatattoo@gmail.com>
2024-02-28 11:06:01 -0300

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Hero from "./components/Hero";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Cotizacion del Dolar",
   description: "Cotizaciones del dolar, euro y otras monedas en Argentina",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="bg-black text-white">
         <Hero /> {children}
       </body>
     </html>
